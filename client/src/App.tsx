de844cb8540d6a0fbf00097745341cf68296cc04
Daniel Kennedy <danielckennedy@hotmail.com>
2019-07-31 06:33:22 -0700

    react_setup setup material-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,87 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme, CssBaseline, Theme } from '@material-ui/core';
+import { ThemeProvider, makeStyles, createStyles } from '@material-ui/styles';
+
+const darkGray = '#292c33';
+const darkerGray = '#141414';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: darkGray,
+      dark: darkerGray,
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+    background: {
+      default: darkGray,
+      paper: darkGray,
+    }
+  },
+  typography: {
+    fontFamily: "'Work Sans', sans-serif",
+  },
+});
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    App: {
+      textAlign: 'center',
+    },
+    AppLogo: {
+      animation: '$AppLogoSpin infinite 20s linear',
+      height: '40vmin',
+      pointerEvents: 'none',
+    },
+    AppHeader: {
+      backgroundColor: '#282c34',
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: 'calc(10px + 2vmin)',
+      color: 'white',
+    },
+    AppLink: {
+      color: '#61dafb',
+    },
+    '@keyframes AppLogoSpin': {
+      from: {
+        transform: 'rotate(0deg)',
+      },
+      to: {
+        transform: 'rotate(360deg)',
+      },
+    }
+  })
+);
 
 const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={classes.App}>
+        <header className={classes.AppHeader}>
+          <img src={logo} className={classes.AppLogo} alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className={classes.AppLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
